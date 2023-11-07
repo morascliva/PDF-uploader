f2a470c091e30b1b08be6ebb141fa007d6b78633
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2023-11-07 14:24:55 +0530

    Create PdfToView.js

---
+++ Code/src/PdfToView.js
@@ -0,0 +1,32 @@
+
+
+
+
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+import './index.css';
+import './styles.css';
+import './App.css';
+function PdfToView({ pdfFile }) {
+  const [numPages, setNumPages] = useState(null);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  return (
+    <div>
+      <Document
+        file={pdfFile}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        ))}
+      </Document>
+    </div>
+  );
+}
+
+export default PdfToView;
+
