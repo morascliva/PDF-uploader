420816442be982d0fe63442449de37bc636e5727
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2023-12-28 00:01:49 +0530

    Update App.test.js

--- Code/src/App.test.js
+++ Code/src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders a sample text', () => {
+  
   render(<App />);
   const sampleTextElement = screen.getByText('This is your sample text');
   expect(sampleTextElement).toBeInTheDocument();
